70b6da29705e1a3fc1b41a1c3024e7ec81777bab
Ahnaf <ahnaf.rasheed.zaki@gmail.com>
2026-08-21 00:51:44 +0600

    visual fixing

--- src/components/LoopingTypingEffect.jsx
+++ src/components/LoopingTypingEffect.jsx
@@ -5,8 +5,8 @@ const LoopingTypingEffect = ({ words, typingSpeed = 80, deletingSpeed = 40, dela
   const [isDeleting, setIsDeleting] = useState(false);
   const [wordIndex, setWordIndex] = useState(0);
 
-  // Cyberpunk theme colors
-  const colors = ['#ff9e2c', '#f92aad', '#00d4f5'];
+  // Cyberpunk theme colors (Cyan, Neon Pink, Synth Purple)
+  const colors = ['#00d4f5', '#f92aad', '#c792ea'];
 
   useEffect(() => {
     const currentWord = words[wordIndex];
